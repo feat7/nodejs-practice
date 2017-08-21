4004d17d849b2ec879680fce0e41190e2975838a
feat7 <vinaykhobragade99@gmail.com>
2017-08-22 02:20:38 +0530

    Working handlebars

--- express/index.js
+++ express/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
 
 app = express();
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+/* Routes */
 app.get('/', function(req, res) {
-  res.json('Hello world');
+  res.render('home');
 });
 
 app.listen(3000, function(err) {
