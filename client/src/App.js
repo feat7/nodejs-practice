b7221d755faccacd49973b722ecef8a64d4d7bf2
feat7 <vinaykhobragade99@gmail.com>
2017-08-24 19:12:15 +0530

    material-ui added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 import {Link} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello React!
-        <Link to="/hello">Go to hello page</Link>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          Hello React!
+          <RaisedButton label={'Hello Page!'} primary={true}
+            containerElement={ <Link to="/hello" /> } />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
